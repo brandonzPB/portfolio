19f0a3fb89bd10f5d54550026debc059b74700da
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2021-02-26 17:58:47 -0800

    Animation

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -16,6 +16,34 @@ const Puzzle = () => {
 export default Puzzle
 
 const IconContainer = styled.div`
+  @keyframes spin {
+    12.5% {
+      transform: scale(1.0125) rotate()
+    }
+
+    25% {
+      transform: scale(1.025) rotate(90deg);
+    }
+
+    37.5% {}
+
+    50% {
+      transform: scale(1.05) rotate(180deg);
+    }
+
+    62.5% {}
+
+    75% {
+      transform: scale(1.075) rotate(270deg);
+    }
+
+    87.5% {}
+
+    100% {
+      transform: scale(1.1) rotate(360deg);
+    }
+  }
+
   margin: 0 auto;
   display: block;
   text-align: left;
@@ -27,11 +55,11 @@ const IconContainer = styled.div`
     z-index: 3;
     background: none;
     border: none;
-    transition: transform 0.3s;
+    transition: animation 0.3s;
   }
 
   .icon:hover {
-    transform: scale(1.1);
+    animation: spin 0.2s ease-out;
   }
 `
 
