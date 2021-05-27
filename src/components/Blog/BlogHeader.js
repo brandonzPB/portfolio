a2b198d5e7fca4ed964817f50918a8e7b825efe5
Brandon Zirulnikoff <zirBrandonWeb@gmail.com>
2021-05-26 20:28:00 -0700

    Epictetus import

--- src/components/Blog/BlogHeader.js
+++ src/components/Blog/BlogHeader.js
@@ -5,45 +5,53 @@ import Img from 'gatsby-image';
 
 import 'fontsource-playfair-display/600.css';
 import 'fontsource-open-sans';
+import epictetus from '../../assets/images/epictetus.jpg';
 
 const BlogHeader = () => {
-  const data = useStaticQuery(graphql`
-    query MyStoic {
-      allFile(filter: {
-        ext: {
-          regex: "/(jpeg)|(jpg)|(png)/"
-        }, 
-        name: {
-          in: "epictetus"
-        }}) {
-          edges {
-            node {
-              childImageSharp {
-                fluid(maxHeight: 1000, quality: 90) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query MyStoic {
+  //     allFile(filter: {
+  //       ext: {
+  //         regex: "/(jpeg)|(jpg)|(png)/"
+  //       }, 
+  //       name: {
+  //         in: "epictetus"
+  //       }}) {
+  //         edges {
+  //           node {
+  //             childImageSharp {
+  //               fluid(maxHeight: 1000, quality: 90) {
+  //                 ...GatsbyImageSharpFluid
+  //               }
+  //             }
+  //           }
+  //         }
+  //     }
+  //   }
+  // `);
 
-  const stoic = {
-    key: 0,
-    fluid: data.allFile.edges[0].node.childImageSharp.fluid,
-    src: data.allFile.edges[0].node.childImageSharp.fluid.src,
-    alt: "Epictetus writing code"
-  };
+  // const stoic = {
+  //   key: 0,
+  //   fluid: data.allFile.edges[0].node.childImageSharp.fluid,
+  //   src: data.allFile.edges[0].node.childImageSharp.fluid.src,
+  //   alt: "Epictetus writing code"
+  // };
 
   return (
-    <HeaderContainer>
+    <HeaderContainer  style={{ 
+      backgroundImage: `url(${epictetus})`,
+      backgroundSize: '100%',
+      backgroundAttachment: 'fixed',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor: 'transparent',
+    }}>
       <BannerContainer>
         <HeaderTextContainer>
           <HeaderText>Learn with the Stoic Programmer</HeaderText>
         </HeaderTextContainer>
 
-        <Epictetus fluid={stoic.fluid} src={stoic.src} alt={stoic.alt} key={0} />
+        {/* <Epictetus fluid={stoic.fluid} src={stoic.src} alt={stoic.alt} key={0} /> */}
       </BannerContainer>
     </HeaderContainer>
   )
@@ -96,6 +104,6 @@ const HeaderTextContainer = styled.div`
 const HeaderText = styled.h1`
   margin: 0 auto;
   font-size: 3.5rem;
-  color: black;
+  color: white;
   font-family: 'Playfair Display', sans-serif;
 `;
