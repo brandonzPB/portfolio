dd321c1a78d89a9d2301b42c9116bc0e9de62b27
Brandon Zirulnikoff <zirBrandonWeb@gmail.com>
2021-05-30 17:49:22 -0700

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,76 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyThinker {
+      allFile(filter: {
+        ext: {
+          regex: "/(png)|(jpg)|(jpeg)/"
+        },
+        name: {
+          in: "thinker"
+        }
+      }) {
+        edges {
+          node {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
 
-export default NotFoundPage
+  const thinker = {
+    fluid: data.allFile.edges[0].node.childImageSharp.fluid,
+    alt: 'An image of the sculpture, The Thinker'
+  };
+
+  return (
+    <Layout>
+      <SEO title="Error 404: Page not found | BZWEB" />
+      <Container>
+        <h1>Page Not Found</h1>
+
+        <PContainer>
+          <p>You may be wondering how you landed in this strange place. Fear not, for nothing is an accident.</p>
+          <p>Perhaps your curiosity got the better of you. How grand! I can recall many instances during which I did just the same.</p>
+          <p>Fortunately for you, there is no consequence for trying to find a page in my website that does not exist.</p>
+          <p>Cheers to your curiosity, fellow creature!</p>
+        </PContainer>
+        
+        <ThinkerImage fluid={thinker.fluid} alt={thinker.alt} />
+      </Container>
+    </Layout>
+  )
+}
+
+export default NotFoundPage;
+
+const Container = styled.div`
+  margin: 2rem auto;
+  text-align: center;
+
+  h1 {
+    margin: 2rem auto;
+  }
+`;
+
+const PContainer = styled.div`
+  margin: 1rem auto;
+`;
+
+const ThinkerImage = styled(Img)`
+  width: 50%;
+  text-align: center;
+  margin: 0 auto;
+`;
