e6ba018bbad567cda705f9675103eada3968b926
Brandon Zirulnikoff <zirBrandonWeb@gmail.com>
2021-06-03 20:09:30 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b18c0e8716a553256d1a.js"
+    "url": "webpack-runtime-4b29bcfb66ae5c498588.js"
   },
   {
     "url": "framework-194c28c051b7aac26109.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a85589c32cc1c2ddd9d5d242d2247615"
+    "revision": "af86d62501c45654b3b369e199ad128c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-931f51b4dc26e1d614ea.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "20e88550ad4c8af31e2fc2763068389b"
+    "revision": "078a7f6c54513d1fc6bda31349774523"
   },
   {
     "url": "polyfill-0af11a10f029a4d27d11.js"
