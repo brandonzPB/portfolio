f273e98756ff558650d8b9d45e9c5c9d8f0bdece
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2021-07-19 22:35:46 -0400

    Added info for Visit_Widget

--- src/pages/Visit_Widget.js
+++ src/pages/Visit_Widget.js
@@ -22,7 +22,10 @@ const Visit_Widget = () => {
 
   const screenshots = images.slice(0, images.length - 1)
 
-  const details = ``
+  const details = `The mobile app of a startup company I am currently working with.
+    I am offering consulting services to Visit Widget, in addition to improving 
+    upon their code base with React Native and Jest unit testing. (Disclaimer: I do not
+      own this app.)`
 
   const stack = [
     "React Native (JavaScript)",
@@ -34,11 +37,24 @@ const Visit_Widget = () => {
 
   const sourceCode = "#"
 
-  const purpose = ``
+  const purpose = `Visit Widget came to me with the interest of working together. After meeting two of the
+    team members and getting to know them very briefly, I quickly wanted to jump on board with the team. The
+    app provides a great service to users and is excellently designed, visually, conceptually, and programatically.
+    I have been improving upon the codebase by writing unit tests in Jest, in addition to improving upon the app
+    itself with React Native.`
 
-  const tech = ``
+  const tech = `I have been updating the app with Jest and React Native. It is my understanding that
+    I will eventually work with Xcode. Fingers crossed!`
 
-  const lessons = ``
+  const lessons = `Working with Visit Widget has taught me an immense amount of knowledge and wisdom, both with 
+    regards to programming and running a business. I've learned how to approach problems more analytically
+    and in terms of the specific variables, functions, and modules required that pose as my constraints. It has
+    improved my ability to use the given toolset and data pool and efficiently develop a strategy for the 
+    goal/functionality I wish to achieve, while keeping edge-cases in mind. It's great practice for the core of 
+    computer programming and debugging: I've built patience, resolve, and the ability to quickly parse through 
+    my code and any error messages to find relevant information. I know unit testing may seem dull and monotonous, 
+    but it is without a doubt one of the best means of practicing and developing your skills as a programmer. I 
+    can't recommend it enough!`
 
   const otherA = {
     link: "/Republic_310",
